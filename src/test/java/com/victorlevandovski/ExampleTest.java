2a112fa176c6871610ead5228ca28729eb266fb1
Victor Levandovski <victorlevandovski@mdmail.nl>
2018-11-13 21:26:39 +0100

    JUnit added

---
+++ src/test/java/com/victorlevandovski/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.victorlevandovski;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ExampleTest {
+
+    @Test
+    void basicTest() {
+        assertEquals(1, 1);
+    }
+}
